7e6a9ee1e2b876de3dfa4165d6ea27b8f99f89e2
yeli041029-hash <ye.li041029@gmail.com>
2026-04-17 15:03:43 +0800

    Delete CPT202 directory

--- CPT202/后端/src/main/java/com/group32/cpt202/LY_heritage/repository/HeritageItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.group32.cpt202.LY_heritage.repository;
-
-import com.group32.cpt202.LY_heritage.entity.HeritageItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HeritageItemRepository extends JpaRepository<HeritageItem, Long> {
-}
