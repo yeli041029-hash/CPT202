5d246c590a56d84d3557e2186406a2b57d98e870
Y2323442 <2737374397@qq.com>
2026-04-17 15:14:05 +0800

    Add files via upload

---
+++ 后端/src/main/java/com/group32/cpt202/group32/Result.java
@@ -0,0 +1,33 @@
+package com.group32;
+
+public class Result<T> {
+    private int code;
+    private String msg;
+    private T data;
+
+    public static <T> Result<T> success(T data) {
+        Result<T> r = new Result<>();
+        r.code = 200;
+        r.msg = "成功";
+        r.data = data;
+        return r;
+    }
+
+    public static <T> Result<T> success() {
+        return success(null);
+    }
+
+    public static <T> Result<T> error(String msg) {
+        Result<T> r = new Result<>();
+        r.code = 500;
+        r.msg = msg;
+        return r;
+    }
+
+    public int getCode() { return code; }
+    public void setCode(int code) { this.code = code; }
+    public String getMsg() { return msg; }
+    public void setMsg(String msg) { this.msg = msg; }
+    public T getData() { return data; }
+    public void setData(T data) { this.data = data; }
+}
